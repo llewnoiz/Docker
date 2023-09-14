20d84379d4d5de128e3fe0548c2f48ced3abd964
song <hyunmin.song@bespinglobal.com>
2023-09-14 16:47:41 +0900

    gitlab token set

--- gitlab/gitlab.rb
+++ gitlab/gitlab.rb
@@ -1,2 +1,3 @@
 external_url 'http://localhost:8929'
 gitlab_rails['initial_root_password'] = File.read('/run/secrets/gitlab_root_password').gsub("\n", "")
+gitlab_rails['initial_shared_runners_registration_token'] = "d1bKPYwKVWvCeSJ_eKcD"
