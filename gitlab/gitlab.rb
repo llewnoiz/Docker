8a39e0dac4e36bb950e2859a1d104f371801c2f0
song <hyunmin.song@bespinglobal.com>
2023-09-21 11:54:05 +0900

    fixed: 설정 변경

--- gitlab/gitlab.rb
+++ gitlab/gitlab.rb
@@ -1,3 +1,3 @@
-external_url 'http://bzcloud.net'
+external_url 'http://gitlab.bzcloud.net'
 gitlab_rails['initial_root_password'] = File.read('/run/secrets/gitlab_root_password').gsub("\n", "")
 gitlab_rails['initial_shared_runners_registration_token'] = "d1bKPYwKVWvCeSJ_eKcD"
